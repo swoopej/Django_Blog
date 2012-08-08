30447e7b1646621d892335a2eac6752c258496e4
swoopej <swoopej@gmail.com>
2012-08-08 16:21:12 -0700

    updating Readme and settings.py

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('julieswoope', 'swoopej@gmail.com'),
+    ('', ''),
 )
 
 MANAGERS = ADMINS
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/julieswoope/Django-Projects/cms/CMS/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'path to your database file here',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -109,8 +109,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/julieswoope/Django-Projects/django-templates",
-    "/Users/julieswoope/Django-Projects/django-templates/coltrane",
+    "path to cms folder here",
+    "path to coltrane folder here",
 )
 
 INSTALLED_APPS = (
